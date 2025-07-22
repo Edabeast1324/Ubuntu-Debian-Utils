625ec8508adcf69b67dbbe85b611156539a4d4f6
edabeast1324 <edabeast1324@gmail.com>
2025-07-22 17:18:00 -0500

    Update guacamole_autoinstaller.sh

--- guacamole_autoinstaller.sh
+++ guacamole_autoinstaller.sh
@@ -1,11 +1,72 @@
 #!/bin/bash
+clear
+echo "Welcome to Apache Guacamole Autoinstaller"
+echo "1) Install for AMD64"
+echo "2) Install for ARM64"
+read -p "Choose an option: " choice
 
-Selection()
-{
-    clear
-    OS="$REPLY"
-    echo "Welcome to Apache Guacamole Autoinstaller for Linux and Debian!"
-    echo "Here are your options:"
-    echo "1. Install for AMD64 devices"
-    echo "2. Install for ARM64 devices"
-}
+if [[ "$choice" == 1 ]]; then
+    echo "Updating System"
+    (sudo apt update && sudo apt upgrade -y) > ~/guacamole_autoinstaller.log
+
+elif [[ "$choice" == 2 ]]; then
+    echo "Updating System"
+    (sudo apt update && sudo apt upgrade -y) > ~/guacamole_autoinstaller.log
+    echo "System up to date"
+    echo "Installing Docker"
+    (curl -fsSL https://get.docker.com -o get-docker.sh) > ~/guacamole_autoinstaller.log
+    (sudo sh get-docker.sh) >> ~/guacamole_autoinstaller.log
+    (sudo usermod -aG docker $USER) >> ~/guacamole_autoinstaller.log
+    echo "Docker is installed"
+
+    echo "Installing Docker-Compose"
+    (sudo apt install -y libffi-dev libssl-dev python3-dev python3 python3-pip) >> ~/guacamole_autoinstaller.log
+    (sudo apt install -y python3 docker-compose) >> ~/guacamole_autoinstaller.log
+    echo "Docker-Compose is installed"
+
+    echo "Creating Docker Compose File"
+    (mkdir -p ~/guacamole && cd ~/guacamole)
+
+    cat << EOF > ~/guacamole/docker-compose.yml
+version: '3.7'
+services:
+  mysql:
+    image: mysql:8.0
+    container_name: mysql
+    restart: unless-stopped
+    environment:
+      MYSQL_ROOT_PASSWORD: root_pass
+      MYSQL_DATABASE: guacamole_db
+      MYSQL_USER: guacamole_user
+      MYSQL_PASSWORD: guac_pass
+    volumes:
+      - ./mysql:/var/lib/mysql
+
+  guacd:
+    image: lscr.io/linuxserver/guacd:latest
+    container_name: guacd
+    restart: unless-stopped
+
+  guacamole:
+    image: flcontainers/guacamole:latest
+    container_name: guacamole
+    restart: unless-stopped
+    depends_on:
+      - guacd
+      - mysql
+    environment:
+      MYSQL_HOST: mysql
+      MYSQL_DATABASE: guacamole_db
+      MYSQL_USER: guacamole_user
+      MYSQL_PASSWORD: guac_pass
+    ports:
+      - "8080:8080"
+    volumes:
+      - ./guac-config:/config
+EOF
+
+    echo "File created"
+    echo "Creating containers and pulling files"
+    (cd ~/guacamole && docker-compose up -d) >> ~/guacamole_autoinstaller.log
+    echo "Fully installed!"
+fi

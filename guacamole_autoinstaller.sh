958173a064a0641b0052f241cccd53cb8a1f79d1
edabeast1324 <edabeast1324@gmail.com>
2025-07-22 15:34:26 -0500

    Add files via upload

---
+++ guacamole_autoinstaller.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+Selection()
+{
+    clear
+    OS="$REPLY"
+    echo "Welcome to Apache Guacamole Autoinstaller for Linux and Debian!"
+    echo "Here are your options:"
+    echo "1. Install for AMD64 devices"
+    echo "2. Install for ARM64 devices"
+}

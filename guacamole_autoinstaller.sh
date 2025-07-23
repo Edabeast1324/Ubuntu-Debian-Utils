f6f47e5838b49a3325c5603e46dcda128a3bfa0b
edabeast1324 <edabeast1324@gmail.com>
2025-07-23 19:51:37 -0400

    Update guacamole_autoinstaller.sh

--- guacamole_autoinstaller.sh
+++ guacamole_autoinstaller.sh
@@ -7,12 +7,16 @@ read -p "Choose an option: " choice
 
 if [[ "$choice" == 1 ]]; then
     echo "Updating System"
+    sleep 2
+    clear
     (sudo apt update && sudo apt upgrade -y)
     clear
     echo "System Up to date"
     sleep 2
     clear
     echo "Installing Docker"
+    sleep 2
+    clear
     (curl -fsSL https://get.docker.com -o get-docker.sh)
     (sudo sh get-docker.sh)
     (sudo usermod -aG docker $USER)
@@ -21,6 +25,8 @@ if [[ "$choice" == 1 ]]; then
     sleep 2
     clear
     echo "Installing Docker-Compose"
+    sleep 2
+    clear
     (sudo apt install -y libffi-dev libssl-dev python3-dev python3 python3-pip)
     (sudo apt install -y python3 docker-compose)
     clear
@@ -28,6 +34,8 @@ if [[ "$choice" == 1 ]]; then
     sleep 2
     clear
     echo "Creating Docker Compose File"
+    sleep 2
+    clear
     (mkdir -p ~/guacamole && cd ~/guacamole)
 
     cat << EOF > ~/guacamole/docker-compose.yml
@@ -74,17 +82,21 @@ EOF
     clear
     echo "Fully installed!"
     echo "Go to: <your device's ip>:8080 to acess"
-fi
+    echo "Or on this device, go to http://localhost:8080"
 
 elif [[ "$choice" == 2 ]]; then
     clear
     echo "Updating System"
+    sleep 2
+    clear
     (sudo apt update && sudo apt upgrade -y)
     clear
     echo "System up to date"
     sleep 2
     clear
     echo "Installing Docker"
+    sleep 2
+    clear
     (curl -fsSL https://get.docker.com -o get-docker.sh)
     (sudo sh get-docker.sh)
     (sudo usermod -aG docker $USER)
@@ -93,6 +105,8 @@ elif [[ "$choice" == 2 ]]; then
     sleep 2
     clear
     echo "Installing Docker-Compose"
+    sleep 2
+    clear
     (sudo apt install -y libffi-dev libssl-dev python3-dev python3 python3-pip)
     (sudo apt install -y python3 docker-compose)
     clear
@@ -100,6 +114,8 @@ elif [[ "$choice" == 2 ]]; then
     sleep 2
     clear
     echo "Creating Docker Compose File"
+    sleep 2
+    clear
     (mkdir -p ~/guacamole && cd ~/guacamole)
 
     cat << EOF > ~/guacamole/docker-compose.yml
@@ -144,8 +160,9 @@ EOF
     sleep 2
     clear
     echo "Creating containers and pulling files"
+    sleep 2
+    clear
     (cd ~/guacamole && docker-compose up -d)
     clear
     echo "Fully installed!"
     echo "Go to: <your device's ip>:8080 to acess"
-fi

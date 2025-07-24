1b66a4b8dcdbe27626515f9519ad520fdcc0df12
edabeast1324 <edabeast1324@gmail.com>
2025-07-24 10:19:31 -0500

    Update kasm_autoinstaller.sh

--- kasm_autoinstaller.sh
+++ kasm_autoinstaller.sh
@@ -1,2 +1,33 @@
 #!/bin/bash
 clear
+echo "Welcome to tha Kasm autoinstaller!
+echo "1) Install for AMD64"
+echo "2) Install for ARM64"
+read -p "Choose an option: " choice
+
+if [[ "$choice" == 1 ]]; then
+clear
+echo "Updating system"
+sleep 2
+clear
+sudo apt update && sudo apt upgrade -y
+clear
+echo "System up to date"
+sleep 2
+clear
+echo "Fetching files"
+sleep 2
+clear
+wget https://kasm-static-content.s3.amazonaws.com/kasm_release_1.17.0.7f020d.tar.gz
+clear
+echo "Files Fetched"
+sleep 2
+clear
+echo "Running install script"
+sleep 2
+clear
+tar -xf kasm_release_1.14.0.01f0e.tar.gz
+cd kasm_release
+sudo bash install.sh
+
+elif [[ "$choice" == 2 ]]; then

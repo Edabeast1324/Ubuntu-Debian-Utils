1827ea3b6efedc40f55fd0d94b2704d1e98f3e72
edabeast1324 <edabeast1324@gmail.com>
2025-07-26 11:26:47 -0500

    Update kasm_autoinstaller.sh

--- kasm_autoinstaller.sh
+++ kasm_autoinstaller.sh
@@ -32,9 +32,31 @@ if [[ "$choice" == "1" ]]; then
     echo "Go to http://<your-ip-address> to access, or http://localhost on this device"
 
 elif [[ "$choice" == "2" ]]; then
-    echo "ARM64 version is not currently supported by this script."
-    echo "Please download and install the ARM64 version manually from:"
-    echo "https://www.kasmweb.com/downloads.html"
+    clear
+    echo "Updating system"
+    sleep 2
+    sudo apt update && sudo apt upgrade -y
+    clear
+    echo "System up to date"
+    sleep 2
+
+    echo "Fetching files"
+    sleep 2
+    wget https://kasm-static-content.s3.amazonaws.com/kasm_release_1.17.0.7f020d.tar.gz
+    clear
+    echo "Files fetched"
+    sleep 2
+
+    echo "Running install script"
+    sleep 2
+    tar -xf kasm_release_1.17.0.7f020d.tar.gz
+    cd kasm_release
+    sudo bash install.sh
+    clear
+    sudo bash install.sh
+    echo "Successfully Installed"
+    echo "Go to http://<your-ip-address> to access, or http://localhost on this device"
+    
 else
     echo "Invalid choice"
     exit 1

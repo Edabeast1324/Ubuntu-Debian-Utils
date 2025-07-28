cb9b4c528e53eeb8725352e7abb075ed86a3368b
edabeast1324 <edabeast1324@gmail.com>
2025-07-28 12:52:23 -0500

    Update Beast_Firewall_installer.sh

--- Beast_Firewall_installer.sh
+++ Beast_Firewall_installer.sh
@@ -1,27 +1,29 @@
 #!/bin/bash
+
+function CheckFail() {
+    status=$?
+    if [ $status -ne 0 ]; then
+        exit 1
+    fi
+}
+
 clear
 echo "Updating System"
 sleep 2
 clear
 sudo apt update && sudo apt upgrade -y
-CheckFail()
+CheckFail
 sleep 2
 clear
 echo "Installing Dependencies"
 sleep 2
 clear
 sudo apt install iptables ip6tables iptables-persistent -y
-CheckFail()
+CheckFail
 sleep 2
 clear
 echo "Fetching Latest Firewall Update"
 sleep 2
 clear
-wget -O "Firewall.sh" "raw.githubusercontent.com/Edabeast1324/Ubuntu-Debian-Utils/refs/heads/main/Firewall-Releases/Latest.sh"
-CheckFail()
-
-function CheckFail() {
-status=$?
-if [ $status -ne 0 ]; then
-    exit 1
-fi
+wget -O "Firewall.sh" "https://raw.githubusercontent.com/Edabeast1324/Ubuntu-Debian-Utils/main/Firewall-Releases/Latest.sh"
+CheckFail

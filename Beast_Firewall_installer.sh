907108dfed6d9bf944994e6b10d8d062b31a132d
edabeast1324 <edabeast1324@gmail.com>
2025-07-28 12:36:56 -0500

    Create Beast_Firewall_installer.sh

---
+++ Beast_Firewall_installer.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+clear
+echo "Updating System"
+sleep 2
+clear
+sudo apt update && sudo apt upgrade -y
+sleep 2
+clear
+echo "Installing Dependencies"
+sleep 2
+clear
+sudo apt install iptables ip6tables iptables-persistent -y
+sleep 2
+clear
+echo "Fetching Latest Firewall Update"
+sleep 2
+clear
+wget -O raw.githubusercontent.com/Edabeast1324/Ubuntu-Debian-Utils/refs/heads/main/Firewall-Releases/Latest.sh
+status=$?
+if [ $status -ne 0 ]; then
+    case $status in
+        1) echo "❌ Generic error occurred.";;
+        4) echo "❌ Network failure. Check your internet."; exit 4;;
+        8) echo "❌ Server error (e.g. 404 Not Found).";;
+        *) echo "❌ Unknown error (code $status).";;
+    esac
+    exit $status
+fi
